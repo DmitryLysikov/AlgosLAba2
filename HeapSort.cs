203b3f1e1fc477467f6e4455ac2004cf3aef4703
DmitryLysikov <129677338+DmitryLysikov@users.noreply.github.com>
2024-03-18 20:34:46 +0500

    Add files via upload

---
+++ HeapSort.cs
@@ -0,0 +1,98 @@
+﻿namespace ALGOS_LABA2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] arr = { 12, 11, 13, 5, 6, 7 };
+
+            //HeapSort ob = new HeapSort();
+            //ob.SortHeap(arr);
+            //ob.PrintArray(arr);
+
+            int[] arr = { 20, 10, 33, 5, 6, 7 };
+
+            HeapSort ob = new HeapSort();
+            ob.SortHeap(arr);
+            ob.PrintArray(arr);
+
+            //int[] arr = { 14, 111, 13, 9, 2, 12 };
+
+            //HeapSort ob = new HeapSort();
+            //ob.SortHeap(arr);
+            //ob.PrintArray(arr);
+
+            //int[] arr = { 112, 141, 103, 50, 66, 75 };
+
+            //HeapSort ob = new HeapSort();
+            //ob.SortHeap(arr);
+            //ob.PrintArray(arr);
+        }
+    }
+    public class HeapSort
+    {
+        public void SortHeap(int[] array)
+        {
+            int n = array.Length;
+            // Построение кучи (перегруппируем массив)
+            BuildMaxHeap(array, n);
+            // Один за другим извлекаем элементы из кучи
+            for (int i = n - 1; i > 0; i--)
+            {
+                // Перемещаем текущий корень в конец
+                Swap(array, 0, i);
+                // вызываем процедуру heapify на уменьшенной куче
+                Heapify(array, i, 0);
+            }
+        }
+
+        private void BuildMaxHeap(int[] array, int n)
+        {
+            for (int i = n / 2 - 1; i >= 0; i--)
+            {
+                Heapify(array, n, i);
+            }
+        }
+        // Процедура для преобразования в двоичную кучу поддерева с корневым узлом i, что является
+        // индексом в arr[]. n - размер кучи
+        private void Heapify(int[] array, int n, int i)
+        {
+            int largest = i;
+            // Инициализируем наибольший элемент как корень
+            int left = 2 * i + 1;
+            int right = 2 * i + 2;
+            // Если левый дочерний элемент больше корня
+            if (left < n && array[left] > array[largest])
+            {
+                largest = left;
+            }
+            // Если правый дочерний элемент больше, чем самый большой элемент на данный момент
+            if (right < n && array[right] > array[largest])
+            {
+                largest = right;
+            }
+            // Если самый большой элемент не корень
+            if (largest != i)
+            {
+                Swap(array, i, largest);
+                // Рекурсивно преобразуем в двоичную кучу затронутое поддерево
+                Heapify(array, n, largest);
+            }
+        }
+
+        private void Swap(int[] array, int i, int j)
+        {
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+
+        /* Вспомогательная функция для вывода на экран массива размера n */
+        public void PrintArray(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n; ++i)
+                Console.Write(arr[i] + " ");
+        }
+    }
+}
---
+++ MergeSort_.cs
@@ -0,0 +1,67 @@
+﻿namespace SortMerge
+{
+    internal class MergeSort_
+    {
+        static void Main(string[] args)
+        {
+            int[] array = {42, 12,112,10,1,13,129};
+            Console.WriteLine("Упорядоченный массив: {0}", string.Join(", ", MergeSort(array)));
+
+            //int[] array = { 420, 1, 11, 100, 9, 12, 19 };
+            //Console.WriteLine("Упорядоченный массив: {0}", string.Join(", ", MergeSort(array)));
+
+            //int[] array = { 1, 0, 112, 10, 2, 3, 500, 44, 50, 90 };
+            //Console.WriteLine("Упорядоченный массив: {0}", string.Join(", ", MergeSort(array)));
+
+            //int[] array = { 1, 12, 3, 10, 88, 33, 229 };
+            //Console.WriteLine("Упорядоченный массив: {0}", string.Join(", ", MergeSort(array)));
+
+            //int[] array = { 400, 5000, 1012, 6000, 231, 153, 10129 };
+            //Console.WriteLine("Упорядоченный массив: {0}", string.Join(", ", MergeSort(array)));
+        }
+
+        static void Merge(int[] array, int lowIndex, int middleIndex, int highIndex)
+        {
+            var left = lowIndex;
+            var right = middleIndex + 1;
+            var tempArray = new int[highIndex - lowIndex + 1];
+            var index = 0;
+
+            while ((left <= middleIndex) && (right <= highIndex))
+            {
+                tempArray[index++] = array[left] < array[right] ? array[left++] : array[right++];
+            }
+
+            while (left <= middleIndex)
+            {
+                tempArray[index++] = array[left++];
+            }
+
+            while (right <= highIndex)
+            {
+                tempArray[index++] = array[right++];
+            }
+
+            Array.Copy(tempArray, 0, array, lowIndex, tempArray.Length);
+        }
+
+        //сортировка слиянием
+        static int[] MergeSort(int[] array, int lowIndex, int highIndex)
+        {
+            if (lowIndex < highIndex)
+            {
+                var middleIndex = (lowIndex + highIndex) / 2;
+                MergeSort(array, lowIndex, middleIndex);
+                MergeSort(array, middleIndex + 1, highIndex);
+                Merge(array, lowIndex, middleIndex, highIndex);
+            }
+
+            return array;
+        }
+
+        static int[] MergeSort(int[] array)
+        {
+            return MergeSort(array, 0, array.Length - 1);
+        }
+    }
+}
